46672d613e6db7df83d715e6c1b318d9d4adc213
sibi xavier <sibixavier@live.com>
2014-12-08 22:21:09 +0530

    submit.php
    Page 1

---
+++ submit.php
@@ -0,0 +1,27 @@
+
+<h1>Please fill in the details</h1>
+
+
+	<form action="testo.php" method="post">
+
+		Student name<input type="text" name="name"><br>
+		Parent name <input type="text" name="parentName"><br>
+		Mobile number<input type="text" name="mobileNumber"><br>
+		<input type="submit">
+	</form>	
+<h1>Please enter name to view details</h1>	
+
+
+	<form action="details.php" method="post">
+
+		Student name<input type="text" name="name"><br>
+
+		<input type="submit">
+	</form>
+
+	<H1>Click to view full details</H1>
+	<form action="full.php" method="post">
+		<input type="submit">
+
+	</form>
+
